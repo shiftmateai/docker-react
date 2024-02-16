d3b4fdda6ce5351a7eb503a8266aa9c38511b1b9
shiftmateai <jasonrcain@outlook.com>
2024-02-16 14:57:58 -0500

    test travis ic to aws

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React now
+          Learn React now - travis-ci
         </a>
       </header>
     </div>
